3a2795fc59bea986b5c394d30e3708933f046ab6
xiaoyu2012 <810936831@qq.com>
2013-09-11 13:00:42 +0800

    911
    2013.9.11

---
+++ 数论/src/NumberOfDivisors/Main.java
@@ -0,0 +1,37 @@
+package NumberOfDivisors;
+import java.util.Scanner;
+
+public class Main {
+	static int n, i, tn;
+	static int[] e;
+	static final int N = 1000;
+	int total = 1;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		e = new int[N];
+		System.out.print(n + "=");
+		tn = n;
+		for(i = 2; i * i <= n; i++)
+			while(n != i) {
+				if(n % i == 0) {
+					System.out.print(i + "*");
+					e[i]++;
+					n = n / i;
+				}
+				else
+					break;				
+			}
+		e[n]++;
+		System.out.println(n);
+		
+		System.out.print(tn + "=");
+		for(int k = 2; k < i; k++)
+			if(e[k] != 0)
+				System.out.print(k +"^" + e[k] + " * ");
+		System.out.println(n + "^" + e[n]);		
+		in.close();
+		
+	}	
+
+}
--- 数论/src/杨辉三角/Main.java
+++ 数论/src/杨辉三角/Main.java
@@ -3,34 +3,31 @@ package Ñî»ÔÈý½Ç;
 import java.util.Scanner;
 
 public class Main {
-	static Scanner in = new Scanner(System.in);
+	
+	static int[][] c;
 	public static void main(String[] args) {
-		long N, M;
-		while(in.hasNext()) {
-			M = in.nextLong();
-			N = in.nextLong();
-			long min = M;
-			long i;
-			if(M < N)
-				System.out.println(-1);
-			else {
-				long mod = M / (N - 1);
-				for(i = mod; i > 0; i--) {		
-					if(M - i * (N -1) > 0 && M - i * (N - 1) != i) {
-						min =  M - i * (N - 1);
-					    break;
-					}
-				}
-				System.out.println(min);
-			}
-			
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		c = new int[n+1][n+1];
+		for(int i = 0; i <= n; i++) {
+			c[i][0] = 1;
+			for(int j = 1; j <= i; j++)
+				c[i][j] = c[i-1][j-1] + c[i-1][j];
+		}
+		
+		for(int i = 0; i <= n; i++) {
+			for(int j = n; j > i; j--)
+				System.out.print(" ");
+			for(int j = 0; j <= i; j++)
+				System.out.print(c[i][j] + " ");	
+			System.out.println();
 			
 		}
 		in.close();
-				
+		
 		}
+
 		
-		
-	}
+}
 
 
